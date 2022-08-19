7a44d4782217d5adf86e104481a4b90f44d8d39f
Rasim Fakhretdinov <frr88@yandex.ru>
2022-08-20 01:00:03 +0500

    Add homework3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1
+Console.WriteLine("Enter two numbers:");
+int NumberFirst = Convert.ToInt32(Console.ReadLine());
+int NumberSecond = Convert.ToInt32(Console.ReadLine());
+    if (NumberFirst > NumberSecond)
+    {
+        Console.WriteLine("Max Number: " + NumberFirst);
+    }
+    else if (NumberFirst < NumberSecond)
+    {
+        Console.WriteLine("Max Number: " + NumberSecond);
+    }
+    else
+    {
+        Console.WriteLine("The numbers are equal!");
+    }
+    
+// Задача 2
+Console.WriteLine("Enter three numbers:");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+int Number2 = Convert.ToInt32(Console.ReadLine());
+int Number3 = Convert.ToInt32(Console.ReadLine());
+    if (Number1 > Number2 && Number1 > Number3)
+    {
+        Console.WriteLine("Max Number: " + Number1);
+    }
+    else if (Number1 < Number2 && Number1 > Number3)
+    {
+        Console.WriteLine("Max Number: " + Number2);
+    }
+    else if (Number1 < Number2 && Number2 < Number3)
+    {
+        Console.WriteLine("Max Number: " + Number3);
+    }
+    else if (Number1 == Number2 && Number1 == Number3)
+    {
+        Console.WriteLine("The three numbers are equal!");
+    }
+
+// Задача 4
+Console.WriteLine("Enter N number:");
+int NumberN = Convert.ToInt32( Console.ReadLine());
+for (int i = 1; i <= NumberN; i += 1)
+{
+if (i % 2 == 0)
+Console.WriteLine("An even numbers: " + i);
+}
