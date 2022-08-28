5dc37ea447650f9bbdeb3bf2d273b683be621351
Rasim Fakhretdinov <frr88@yandex.ru>
2022-08-28 16:40:13 +0500

    2 homework add

--- Program.cs
+++ Program.cs
@@ -1,59 +1,18 @@
-﻿// Задача 1
-Console.WriteLine("Enter two numbers:");
-int NumberFirst = Convert.ToInt32(Console.ReadLine());
-int NumberSecond = Convert.ToInt32(Console.ReadLine());
-    if (NumberFirst > NumberSecond)
-    {
-        Console.WriteLine("Max Number: " + NumberFirst);
-    }
-    else if (NumberFirst < NumberSecond)
-    {
-        Console.WriteLine("Max Number: " + NumberSecond);
-    }
-    else
-    {
-        Console.WriteLine("The numbers are equal!");
-    }
+﻿// Задачи 10 и 13
+Console.WriteLine("Enter any number from 0 to 999:");
+int AnyNumber = Convert.ToInt32(Console.ReadLine());
 
-// Задача 2
-Console.WriteLine("Enter three numbers:");
-int Number1 = Convert.ToInt32(Console.ReadLine());
-int Number2 = Convert.ToInt32(Console.ReadLine());
-int Number3 = Convert.ToInt32(Console.ReadLine());
-    if (Number1 > Number2 && Number1 > Number3)
+    if (AnyNumber <= 9)
     {
-        Console.WriteLine("Max Number: " + Number1);
+        Console.WriteLine("This is a single digit number: " + AnyNumber);
     }
-    else if (Number1 < Number2 && Number1 > Number3)
+    else if (AnyNumber > 9 && AnyNumber < 100)
     {
-        Console.WriteLine("Max Number: " + Number2);
+        Console.WriteLine("Second digit: " + (AnyNumber % 10));
+        Console.WriteLine("Third digit is missing");
     }
-    else if (Number1 < Number2 && Number2 < Number3)
+    else if (AnyNumber >= 100)
     {
-        Console.WriteLine("Max Number: " + Number3);
+        Console.WriteLine("Second digit: " + ((AnyNumber / 10) % 10));
+        Console.WriteLine("Third digit:" + (10 - (AnyNumber - (AnyNumber / 10) % 10)));
     }
-    else if (Number1 == Number2 && Number1 == Number3)
-    {
-        Console.WriteLine("The three numbers are equal!");
-    }
-
-// Задача 3
-Console.WriteLine("Enter one numbers:");
-int NumberOne = Convert.ToInt32(Console.ReadLine());
-    if (NumberOne % 2 == 0)
-    {
-        Console.WriteLine("An even Number: " + NumberOne);
-    }
-    else
-    {
-        Console.WriteLine("Odd Number: " + NumberOne);
-    }
-
-// Задача 4
-Console.WriteLine("Enter N number:");
-int NumberN = Convert.ToInt32( Console.ReadLine());
-for (int i = 1; i <= NumberN; i += 1)
-{
-if (i % 2 == 0)
-Console.WriteLine("An even numbers: " + i);
-}
